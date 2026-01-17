99bd15a2ab3fc79867929d9627b75c485e7babd9
Logan Rose <logprose@umich.edu>
2026-01-16 21:21:33 -0500

    chore: init commit, feat: parsing html

---
+++ README.md
@@ -0,0 +1,14 @@
+## Rungine
+IDF-TF implemented in Rust!
+
+
+### Left to implement
+[ ] Parse directories
+[ ] Parse files (Tokenizer??? How do I delimit?)
+[ ] Create index
+[ ] Write to DB? (NoSQL?)
+[ ] ???
+
+
+### Questions
+- Do I want to grab hrefs to further expand on the accuracy? i.e. using number of links to as an indicator
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::read_to_string;
+use std::collections::HashMap;
+use scraper::Selector;
+use scraper::Html;
+
+
+
+
+fn main() {
+
+    let file = read_to_string("./tests/std-fs-file.html").unwrap();
+    println!("{}", file);
+    let document = Html::parse_document(file.as_str());
+    // println!("{:?}", document);
+    let paragraph = Selector::parse("p").unwrap();
+    println!("{:?}", paragraph);
+
+    
+}
+
+struct Parser;
+
+// impl parse for Parser {
+//     fn parse(file: String) -> HashMap<String, u16> {
+//         todo!()
+//     }
+// }
+
+fn html_parser() {
+    todo!()
+}
+
+fn iter_dirs() {
+    todo!()
+}
+
+fn read_file(file_name: String) -> Vec<String> {
+    todo!()
+}
