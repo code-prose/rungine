4e4899c9753811ddc9df12a0c89f707dc3339fcd
Logan Rose <logprose@umich.edu>
2026-01-21 22:33:02 -0500

    feat: Indexed enum to see if I can avoid re-indexing for future runs? or do I need to re-index?

--- src/main.rs
+++ src/main.rs
@@ -25,12 +25,15 @@ fn main() -> io::Result<()> {
         
         let tfidf = Indexer::create_index(hmap, num_words);
         documents.push(Document{path: fp.to_str().unwrap().to_string(), tfidfs: tfidf});
-
     }
 
     Ok(())
 }
 
+enum Indexed {
+    Indexed,
+    Unindexed
+}
 
 struct TFIDF {
     term: String,
@@ -40,7 +43,8 @@ struct TFIDF {
 
 struct Document {
     path: String,
-    tfidfs: Vec<TFIDF>
+    tfidfs: Vec<TFIDF>,
+    // indexed: Indexed
 }
 
 struct Indexer;
